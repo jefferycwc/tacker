2596a1472931c8c732cdb05fba45827fd977d5a7
jeffery71 <jefferynthu@gmail.com>
2020-11-12 16:20:25 +0800

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
+echo "record all files"
 python setup.py install --record files.txt
+echo "rm all files"
 cat files.txt | xargs sudo rm -rf
+echo "start installing"
 python setup.py install
+echo "stop tacker service"
 systemctl stop tacker
+echo "start tacker service"
 systemctl start tacker
